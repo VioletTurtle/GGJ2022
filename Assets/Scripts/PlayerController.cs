71d6cb17b46a5a3333b295f12eec696dcfcd43b7
Chloe Baxter <Abaxter9@students.kennesaw.edu>
2022-01-28 21:51:07 -0500

    Player Movement prototype
    Simple player movement with jumping for testing purposes.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigBody;
+    public float Speed = 5;
+    public float jumpSpeed = 20f;
+    bool isGrounded = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    void Move()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float moveBy = hor * Speed;
+        rigBody.velocity = new Vector2(moveBy, rigBody.velocity.y);
+    }
+
+    void Jump()
+    {
+        if (isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                
+                rigBody.AddForce(transform.up * jumpSpeed);
+                isGrounded = false;
+
+            }
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+}
