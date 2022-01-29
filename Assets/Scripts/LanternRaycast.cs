af03eb44775438d47874b95523ead7488d78db49
ian-mcconnell <45210935+ian-mcconnell@users.noreply.github.com>
2022-01-29 09:46:09 -0500

    Raycast Bugfix
    Fixed a bug causing the platform to stay deactivated even when hit

--- Assets/Scripts/LanternRaycast.cs
+++ Assets/Scripts/LanternRaycast.cs
@@ -11,13 +11,14 @@ public class LanternRaycast : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         RaycastHit2D[] hits = new RaycastHit2D[10];
         for (int i = 0; i < 10; i++)
         {
+            //Quaternion math:
+            //https://answers.unity.com/questions/146975/how-to-raycast-on-45-degree.html
             RaycastHit2D hit = Physics2D.Raycast(transform.position, Quaternion.Euler(0, 0, -10f + 2f * i) * transform.right, 12f);
-            //Vector3 forward = transform.TransformDirection(Vector3.right) * 12;
             Debug.DrawRay(transform.position, Quaternion.Euler(0, 0, -10f + 2f * i) * transform.right * 12, Color.red);
             hits.SetValue(hit, i);
         }
