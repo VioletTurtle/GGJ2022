beac698ebc4a02ebe6ac83471e4d9fb05ad98433
VioletTurtle <55772041+VioletTurtle@users.noreply.github.com>
2022-01-30 12:37:29 -0500

    Volume Scripting

--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class MainMenuUI : MonoBehaviour
 {
     public GameObject UnlitBack;
     public GameObject LitBack;
+    public AudioMixer audioMixer;
+    public string MixerName;
     public void PlayGame()
     {
         SceneManager.LoadScene("Test", LoadSceneMode.Single);
@@ -14,6 +17,11 @@ public class MainMenuUI : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat(MixerName, volume);
     }
 
     private void Start()
--- Assets/Scripts/PauseMenuUI.cs
+++ Assets/Scripts/PauseMenuUI.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class PauseMenuUI : MonoBehaviour
 {
     public GameObject PauseMenu;
+    public AudioMixer audioMixer;
+    public string MixerName;
 
     // Update is called once per frame
     void Update()
@@ -27,5 +30,10 @@ public class PauseMenuUI : MonoBehaviour
     public void MainMenu()
     {
         SceneManager.LoadScene("Main Menu", LoadSceneMode.Single);
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat(MixerName, volume);
     }
 }
