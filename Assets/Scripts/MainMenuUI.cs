48575f78871ea1cfb6ae006a9caaa8514455871d
VioletTurtle <55772041+VioletTurtle@users.noreply.github.com>
2022-01-29 23:37:45 -0500

    Win

--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -5,12 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuUI : MonoBehaviour
 {
+    public GameObject UnlitBack;
+    public GameObject LitBack;
     public void PlayGame()
     {
-        SceneManager.LoadScene("Level", LoadSceneMode.Single);
+        SceneManager.LoadScene("Test", LoadSceneMode.Single);
     }
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    private void Start()
+    {
+        if(GameObject.FindGameObjectWithTag("WinDetect").GetComponent<MenuImageChanger>().hasWon == false)
+        {
+            UnlitBack.SetActive(true);
+            LitBack.SetActive(false);
+        }
+        else if (GameObject.FindGameObjectWithTag("WinDetect").GetComponent<MenuImageChanger>().hasWon == true)
+        {
+            UnlitBack.SetActive(false);
+            LitBack.SetActive(true);
+        }
     }
 }
