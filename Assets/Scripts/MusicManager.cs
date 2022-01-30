28e8f9daa7ef775802e29252139f6b8025b337cb
ian-mcconnell <45210935+ian-mcconnell@users.noreply.github.com>
2022-01-29 21:22:17 -0500

    Created MusicController script

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    private GameObject player;
+    private float playerHeight;
+
+    AudioSource[] audioSources = new AudioSource[6];
+    public float thirdLayerHeight = 20f;
+    public float fourthLayerHeight = 40f;
+    public float fifthLayerHeight = 60f;
+    public float sixthLayerHeight = 80f;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        playerHeight = player.transform.position.y;
+
+        int index = 0;
+        foreach (AudioSource aSource in GetComponents<AudioSource>())
+        {
+            audioSources.SetValue(aSource, index);
+            index++;
+        }
+        foreach (AudioSource aSource in audioSources)
+        {
+            aSource.Play();
+        }
+    }
+
+    
+    void Update()
+    {
+        playerHeight = player.transform.position.y;
+
+        if (playerHeight >= sixthLayerHeight)
+        {
+            audioSources[5].volume = 1.0f;
+        }
+        else 
+        {
+            audioSources[5].volume = 0.0f;
+        }
+        if (playerHeight >= fifthLayerHeight)
+        {
+            audioSources[4].volume = 1.0f;
+        }
+        else
+        {
+            audioSources[4].volume = 0.0f;
+        }
+        if (playerHeight >= fourthLayerHeight)
+        {
+            audioSources[3].volume = 1.0f;
+        }
+        else
+        {
+            audioSources[3].volume = 0.0f;
+        }
+        if (playerHeight >= thirdLayerHeight)
+        {
+            audioSources[2].volume = 1.0f;
+        }
+        else
+        {
+            audioSources[2].volume = 0.0f;
+        }
+    }
+}
